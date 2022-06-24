c4282873231e62eb0f664cbe0e598ef46d07dccb
Luis Felipe Nascimento Santana <93007295+LuisFelipeVTX9@users.noreply.github.com>
2022-06-24 20:43:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const target = document.querySelectorAll('[data-anime]');
+
+const animationClass = 'animate';
+
+function animeScroll(){
+	const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+	target.forEach(function(element){
+		if((windowTop) > element.OffsetTop){
+			element.classList.add(animationClass);
+		}else{
+			element.classList.remove(animationClass);
+		}
+
+
+	})
+}
+
+	window.addEventListener('scroll', function(){
+		animeScroll();
+	})*/
